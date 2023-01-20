f2fd3cf59af1fa11bddbccf05234d5ffec4e57b3
User <user@its.me>
2023-01-20 11:36:41 +0100

    forms with Spring

---
+++ src/main/java/online/javafun/forms/CalculatorService.java
@@ -0,0 +1,14 @@
+package online.javafun.forms;
+
+public class CalculatorService {
+    static double calculate(double a, double b, String operator) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> {
+                throw new IllegalArgumentException("Nieobsługiwany operator matematyczny");}
+        };
+    }
+}
